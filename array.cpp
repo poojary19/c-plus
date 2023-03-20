e4ede1e6c689f094c520926e76915601b50e18ca
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-03-20 19:39:36 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>  
+using namespace std;  
+int main()   
+{  
+    char char_array[] = {'J','A','V','A','T','P','O','I','N','T'};  
+    int array_size = sizeof(char_array) /  
+    sizeof(char);  
+    string j = "";  
+    int i;  
+    for(i = 0; i < array_size; i++)  
+   {  
+    j = j + char_array[i];  
+    }  
+    cout << j <<endl;  
+}   
