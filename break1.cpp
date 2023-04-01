aab0062c2e7ad14a289fc83125f6c101f10ecd47
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-04-01 20:45:01 +0530

    Add files via upload

---
+++ break1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  
+using namespace std;  
+int main() {  
+      for (int i = 2; i <= 20; i++)    
+          {    
+              if (i == 5)    
+              {    
+                  break;    
+              }    
+        cout<<i<<"\n";    
+          }    
+}  
